f204e605527912d4a63adda83aed3ac877caa9c3
leearam <dldkfka331@softzam.com>
2022-07-13 09:34:43 +0900

    Updates

---
+++ precache-manifest.280465331fe0042c77ab7d7edf572a59.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9235e3a7af98bde087eb",
+    "url": "/react-for-beginners/static/js/main.9235e3a7.chunk.js"
+  },
+  {
+    "revision": "a0d26beb2119e251ceeb",
+    "url": "/react-for-beginners/static/js/1.a0d26beb.chunk.js"
+  },
+  {
+    "revision": "93ab4fdb2861308b05c8",
+    "url": "/react-for-beginners/static/js/runtime~main.93ab4fdb.js"
+  },
+  {
+    "revision": "c1efcad838ca77bae32eb28a4aab7123",
+    "url": "/react-for-beginners/index.html"
+  }
+];
